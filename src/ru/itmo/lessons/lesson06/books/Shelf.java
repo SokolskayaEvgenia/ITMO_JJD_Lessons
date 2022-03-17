91077897a02da6763b216638303d8c642ca27191
Evgenia Sokolskaya <evgeniagrushina@gmail.com>
2022-03-17 18:52:20 +0300

    добавлены сеттер и геттер цвета полки, метод добавления нескольких книг

--- src/ru/itmo/lessons/lesson06/books/Shelf.java
+++ src/ru/itmo/lessons/lesson06/books/Shelf.java
@@ -1,26 +1,43 @@
 package ru.itmo.lessons.lesson06.books;
 
+import java.util.Arrays;
+
 public class Shelf {
     private String color = "white";// цвет полки
     // книга
-    public Book[] books = new Book[10];
+    private Book[] books = new Book[4];
 
     public Book[] getBooks() {
         return books;
     }
 
+    public void setColor(String color) {
+        if (color == null || color.length() < 4) {
+            throw new IllegalArgumentException("В названии цвета должно быть не менее 4 символов");
+        }
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
     //метод позволяет добавить одну книгу на полку
-    public void addBook(Book book){
+    public void addBook(Book book) {
         for (int i = 0; i < books.length; i++) {
-            if (books[i]==null){
+            if (books[i] == null) {
                 books[i] = book;
                 return;
             }
         }
-        System.out.println("Нет места");
+        System.out.println("Книге '" + book.getTitle() + "' автора " + book.getAuthor().getFullName() + " не хватило места на полке ");
     }
-    // метод добавления нескольких книг на полку
-    public void addBook(Book... books){
 
+    // метод добавления нескольких книг на полку
+    public void addBook(Book... books) {
+        System.out.println(Arrays.toString(books));
+        for (Book book : books) {
+            addBook(book); // вызов метода addBook(Book book)
+        }
     }
 }
